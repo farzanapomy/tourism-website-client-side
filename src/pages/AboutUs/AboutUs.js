e171f80eae5f21c6c11f7ecfc9266afa4cee78d5
farzanapomy <farzanapomy56@gmail.com>
2021-11-01 23:39:08 +0600

    last

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -4,13 +4,13 @@ const AboutUs = () => {
     return (
         <div id='aboutUs' className='container mb-3 bg-dark text-light p-5'>
             <h1 className='py-3'>Learn More About Our Company</h1>
-            <div className="row row-col-12  my-4">
-                <div className="col ">
+            <div className="row  my-4">
+                <div className="col-lg-6 col-md-12">
                     <p>We have been moving excellent encounters for a considerable length of time through our cutting-edge planned occasion bundles and other fundamental travel administrations. We rouse our clients to carry on with a rich life, brimming with extraordinary travel encounters.
 
                         Through our exceptionally curated occasion bundles, we need to take you on an adventure where you personally enjoy the stunning magnificence of America and far-off terrains. We need you to observe sensational scenes that are a long way past your creative ability.</p>
                 </div>
-                <div className="col">
+                <div className="col-lg-6 col-md-12">
                     <img src={img} className='img-fluid w-75' alt="" />
                 </div>
             </div>
